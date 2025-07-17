5ac3930f5fbd235b867f04197307e536009cee29
Matildehdz4811 <24310649@utcalvillo.edu.mx>
2025-07-17 11:49:09 -0600

    Subida de proyecto

---
+++ SmartParking/src/main/java/com/mycompany/smartparking/SmartParking.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.smartparking;
+
+/**
+ *
+ * @author jesus
+ */
+public class SmartParking {
+
+    public static void main(String[] args) {
+        login v = new login();
+        v.setVisible(true);
+    }
+}
